f582cc4fe118c42e241f78b4b62bcdfbd87cf992
chris <chris@c-mart.in>
2023-12-10 13:30:41 -0700

    Add message passing system between parts of extension, not wired up yet

--- src/elm/Common.elm
+++ src/elm/Common.elm
@@ -23,3 +23,78 @@ encodeModel model =
           , Json.Encode.list Json.Encode.string model.domainsToRedirect
           )
         ]
+
+
+type alias Exception =
+    { domain : String }
+
+
+type MessageFromInterceptPage
+    = RequestModel
+    | NewException Exception
+
+
+encodeMessageFromInterceptPage : MessageFromInterceptPage -> Json.Encode.Value
+encodeMessageFromInterceptPage message =
+    case message of
+        RequestModel ->
+            Json.Encode.object [ ( "tag", Json.Encode.string "request-model" ) ]
+
+        NewException exception ->
+            Json.Encode.object
+                [ ( "tag", Json.Encode.string "new-exception" )
+                , ( "exception"
+                  , Json.Encode.object
+                        [ ( "domain", Json.Encode.string exception.domain ) ]
+                  )
+                ]
+
+
+decodeMessageFromInterceptPage : Json.Decode.Decoder MessageFromInterceptPage
+decodeMessageFromInterceptPage =
+    let
+        decode tag =
+            case tag of
+                "request-model" ->
+                    Json.Decode.succeed RequestModel
+
+                "new-exception" ->
+                    Json.Decode.at [ "exception", "domain" ] Json.Decode.string
+                        |> Json.Decode.map Exception
+                        |> Json.Decode.map NewException
+
+                _ ->
+                    Json.Decode.fail "Unrecognized message tag"
+    in
+    Json.Decode.field "tag" Json.Decode.string
+        |> Json.Decode.andThen decode
+
+
+type MessageFromBackgroundScript
+    = SendModel Model
+
+
+encodeMessageFromBackgroundScript : MessageFromBackgroundScript -> Json.Encode.Value
+encodeMessageFromBackgroundScript message =
+    case message of
+        SendModel model ->
+            Json.Encode.object
+                [ ( "tag", Json.Encode.string "send-model" )
+                , ( "model", encodeModel model )
+                ]
+
+
+decodeMessageFromBackgroundScript : Json.Decode.Decoder MessageFromBackgroundScript
+decodeMessageFromBackgroundScript =
+    let
+        decode tag =
+            case tag of
+                "send-model" ->
+                    Json.Decode.field "model" modelDecoder
+                        |> Json.Decode.map SendModel
+
+                _ ->
+                    Json.Decode.fail "Unrecognized message tag"
+    in
+    Json.Decode.field "tag" Json.Decode.string
+        |> Json.Decode.andThen decode
---
+++ tests/Tests.elm
@@ -0,0 +1,73 @@
+module Tests exposing (messageRoundTrip, modelRoundTrip)
+
+import Common as C
+import Expect
+import Json.Decode
+import Test exposing (..)
+
+
+testModel : C.Model
+testModel =
+    C.Model [ "reddit.com" ]
+
+
+testException : C.Exception
+testException =
+    C.Exception "facebook.com"
+
+
+modelRoundTrip : Test
+modelRoundTrip =
+    test "Round trip model" <|
+        \_ ->
+            Expect.equal
+                (Ok testModel)
+                (testModel
+                    |> C.encodeModel
+                    |> Json.Decode.decodeValue C.modelDecoder
+                )
+
+
+messageRoundTrip : Test
+messageRoundTrip =
+    let
+        messagesFromInterceptPage =
+            [ { m = C.RequestModel
+              , d = "request model"
+              }
+            , { m = C.NewException testException
+              , d = "new exception"
+              }
+            ]
+
+        messagesFromBGScript =
+            [ { m = C.SendModel testModel
+              , d = "send model"
+              }
+            ]
+    in
+    describe "Round trip messages" <|
+        List.concat
+            [ List.map
+                (\message ->
+                    test message.d <|
+                        \_ ->
+                            Expect.equal (Ok message.m)
+                                (message.m
+                                    |> C.encodeMessageFromInterceptPage
+                                    |> Json.Decode.decodeValue C.decodeMessageFromInterceptPage
+                                )
+                )
+                messagesFromInterceptPage
+            , List.map
+                (\message ->
+                    test message.d <|
+                        \_ ->
+                            Expect.equal (Ok message.m)
+                                (message.m
+                                    |> C.encodeMessageFromBackgroundScript
+                                    |> Json.Decode.decodeValue C.decodeMessageFromBackgroundScript
+                                )
+                )
+                messagesFromBGScript
+            ]
