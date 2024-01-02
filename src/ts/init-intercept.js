574a8b255687e2aea2f7f3894ec4cab11c45fffd
chris <chris@c-mart.in>
2024-01-02 13:22:45 -0800

    actually send message from intercept page

--- src/ts/init-intercept.js
+++ src/ts/init-intercept.js
@@ -9,6 +9,10 @@ browser.runtime.sendMessage({ tag: "request-model" });
 
 browser.runtime.onMessage.addListener(receiveMessage);
 
+app.ports.sendMessage.subscribe(function (message) {
+  browser.runtime.sendMessage(message);
+});
+
 function receiveMessage(message) {
   app.ports.receiveMessage.send(message);
 }
